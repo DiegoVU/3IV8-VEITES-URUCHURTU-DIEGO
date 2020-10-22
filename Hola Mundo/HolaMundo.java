eff794b5edf394d6deae1b7d3c97baadc9416b4f
DiegoVU <72159423+DiegoVU@users.noreply.github.com>
2020-10-21 21:20:20 -0500

    Add files via upload

---
+++ Hola Mundo/HolaMundo.java
@@ -0,0 +1,21 @@
+class HolaMundo{   
+// aqui es donde va a estar todo nurestro codigo de programacion
+// esto es el cuerpo de la programación
+
+//clase principal
+
+public static void main(String[] args){
+    /*
+Este es el metodo principal de cualquier clase de java
+
+en el cual se especifica que va a poder recibir cadenas como argumentos
+
+Los argumentos son cualquier tipo de valor que pueda recibir este metodo principal, aquí es donde nosotros especificamos todo el cuerpo del algoritmo
+*/
+
+System.out.print("HolaMundo --, ya no eres kawai");
+//Esto es lo mismo que
+//Escribir "HolaMundo ya no eres kawai"
+
+}
+}
